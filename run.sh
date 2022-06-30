df6c7d9ccf5b41ba98113917b685d2044ffaef42
Jeff Milton <milton@lajollalabs.com>
2022-06-30 12:56:40 +0000

    gmap and bedtools endpionts

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+export PYTHONPATH=/gmap/gmap-gsnap-RESTful-services:/gmap/gmap-gsnap-RESTful-services/gscript/gmapr:/gmap/gmap-gsnap-RESTful-services/gscript/bedtools
+export PATH=$PATH:/gmap/gmap-gsnap-RESTful-services/gscript
 python3 manage.py runserver 0.0.0.0:8000
